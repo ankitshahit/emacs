21f373c86383e34cce2e1d3efc218dddbe6e8b01
Ankit <ankitshahit@users.noreply.github.com>
2023-07-12 13:11:47 +0530

    added directory

--- init.el
+++ init.el
@@ -67,7 +67,7 @@
 ;; Recipe is always a list
 ;; Install via Guix if length == 1 or :guix t is present
 
-(setq backup-directory-alist '((".*" . ,temporary-file-directory))
+(setq backup-directory-alist '((".*" . "~/.emacs.d/backup"))
       backup-by-copying t    ; Don't delink hardlinks
       version-control t      ; Use version numbers on backups
       delete-old-versions t  ; Automatically delete excess backups
@@ -75,7 +75,7 @@
       kept-old-versions 5    ; and how many of the old
       )
 (setq auto-save-file-name-transforms
-`((".*" ,temporary-file-directory t)))
+`((".*" "~/.emacs.d/backup"  t)))
 (setq delete-by-moving-to-trash t)
 ;; Initialize package sources
 (defvar dw/guix-emacs-packages '()
